dd3824e7e7a5ee3e0d5623f6187d577e1a87a288
kyracm <kyracm@kyracm-pc.local>
2019-03-08 17:54:16 -0500

    Two sample superiority test implemented

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+function NormSInv(p) {
+    var a1 = -39.6968302866538, a2 = 220.946098424521, a3 = -275.928510446969;
+    var a4 = 138.357751867269, a5 = -30.6647980661472, a6 = 2.50662827745924;
+    var b1 = -54.4760987982241, b2 = 161.585836858041, b3 = -155.698979859887;
+    var b4 = 66.8013118877197, b5 = -13.2806815528857, c1 = -7.78489400243029E-03;
+    var c2 = -0.322396458041136, c3 = -2.40075827716184, c4 = -2.54973253934373;
+    var c5 = 4.37466414146497, c6 = 2.93816398269878, d1 = 7.78469570904146E-03;
+    var d2 = 0.32246712907004, d3 = 2.445134137143, d4 = 3.75440866190742;
+    var p_low = 0.02425, p_high = 1 - p_low;
+    var q, r;
+    var retVal;
+
+    if ((p < 0) || (p > 1))
+    {
+        alert("NormSInv: Argument out of range.");
+        retVal = 0;
+    }
+    else if (p < p_low)
+    {
+        q = Math.sqrt(-2 * Math.log(p));
+        retVal = (((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+    }
+    else if (p <= p_high)
+    {
+        q = p - 0.5;
+        r = q * q;
+        retVal = (((((a1 * r + a2) * r + a3) * r + a4) * r + a5) * r + a6) * q / (((((b1 * r + b2) * r + b3) * r + b4) * r + b5) * r + 1);
+    }
+    else
+    {
+        q = Math.sqrt(-2 * Math.log(1 - p));
+        retVal = -(((((c1 * q + c2) * q + c3) * q + c4) * q + c5) * q + c6) / ((((d1 * q + d2) * q + d3) * q + d4) * q + 1);
+    }
+
+    return retVal;
+}
+
 $(document).ready(function () {
 
     var alpha = 0.05;
@@ -14,6 +51,9 @@ $(document).ready(function () {
     var meandiff = 0;
     var absmeandiff = 0; 
     var terms2;
+    var n_a = 0;
+    var n_b = 0;
+    var n_total = 0; 
 
     var nullhypothesishtmldiv = '<div class="nullHypothesis">&mu;: <br> <textarea class="textNum" id="nullHyp" placeholder="0"></textarea> </div>'
     var althypothesishtmldiv = '<div class="altHypothesis">&mu;<sub>0</sub>: <br> <textarea class="textNum" id="altHyp" placeholder="0"></textarea> </div>'
@@ -71,6 +111,26 @@ $(document).ready(function () {
         {
             "term": "Randomization",
             "definition": "The random assignment of patients into treatment groups. If subjects are assigned randomly into these groups, the study is a randomized controlled trial."
+        },
+        {
+            "term": "Phase I Clinical Trial",
+            "definition": "The goal of this type of trial is to assess toxicity; determine drug’s pharmacokinetic and pharmacodynamic profiles; and determine doses resulting in sufficient biological levels of the drug."
+        },
+        {
+            "term": "Phase II Clinical Trial",
+            "definition": "The goal of this type of trial is to determine the drug’s short-term risks, and to examine preliminary effectiveness of the drug."
+        },
+        {
+            "term": "Phase III Clinical Trial",
+            "definition": "The goal of this type of trial is to determine the drug’s effectiveness; determine long term drug safety, and confirm Phase II findings. "
+        }, 
+        {
+            "term": "Dose Limiting Toxicity (DLT)",
+            "definition": "Treatment related non-hematological toxicity of grade 3 or higher, or treatment related hematological toxicity of grade 4 or higher"
+        },
+        {
+            "term": "Maximum Tolerated Dose (MTD)",
+            "definition": "Usually, the therapeutic effect of a drug is believed to increase with dose. However, the dose of a study drug a subject can receive is often limited by toxicity. Hence the target dose is the maximum dose that is tolerable. The maximum tolerated dose is the dose such that Pr(dose limiting toxicity at MTD)=&gamma;, where &gamma; is the target DLT rate"
         }
 
     ]
@@ -184,17 +244,15 @@ $(document).ready(function () {
             range.on('input', function () {
                 power = this.value;
                 $(this).next(value).html(this.value + "%");
-                console.log(power);
+                beta = (100 - power) / 100.0;        
             });
         });
-        beta = (100 - power) / 100.0;
-        console.log(beta);
+
     };
 
     $(function () {
         $(".textAreaA").keypress(function (e) {
             var code = (e.keyCode ? e.keyCode : e.which);
-            //alert(code);
             if (code == 13) {
                 trtGroupAName = $('.textAreaA').val();
                 $('.trtGroup_A').empty();
@@ -207,6 +265,25 @@ $(document).ready(function () {
     $(function () {
         $("#resetOptions").click(function () {
             location.reload();
+        });
+    });
+
+    $(function () {
+        $("#simulate").click(function () {
+            $('#samplesize').empty();
+            if (testType == "sup2") {
+                let zbeta = NormSInv(1-beta)
+                let zalphaover2 = NormSInv(1-(alpha/2))
+                let kplusone = parseFloat(nratio) + 1;
+                let naNum = kplusone * Math.pow(sigma,2) * Math.pow((zbeta+zalphaover2),2);
+                let naden = Math.pow(meandiff,2)
+                
+                n_a=Math.ceil(naNum/naden);
+                n_b=Math.ceil(n_a/parseFloat(nratio));
+                n_total = n_a+n_b;
+            }
+            $('#samplesize').append("<h2>Sample Size Breakdown</h2>")
+            $('#samplesize').append("<p id = 'sampsizenum'>Your total sample size should be greater than or equal to " + n_total + ". You need at least " + n_a +" subjects for " + trtGroupAName+ " and at least " + n_b + " subjects for " + trtGroupBName +". Note: sample size calculations have been rounded up to be conservative with these estimates.</p>")
         });
     });
 
